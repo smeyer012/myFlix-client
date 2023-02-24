4fb7d35ee9e3c87e97d22457131e489124d02b66
smeyer012 <115064549+smeyer012@users.noreply.github.com>
2023-02-24 16:11:19 -0600

    introduces useEffect to get movie data from API and adjusts keys accordingly

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,39 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            Title: "The Silence of the Lambs",
-            ImagePath:
-                "https://m.media-amazon.com/images/I/411K6E7KRDL._AC_.jpg",
-            Description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.",
-            Director: "Jonathan Demme",
-            Genre: "Thriller"
-        },
-        {
-            id: 2,
-            Title: "Fight Club",
-            ImagePath:
-                "https://m.media-amazon.com/images/I/51D6HRAMBSL.jpg",
-            Description: "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
-            Director: "David Fincher",
-            Genre: "Action"
-        },
-        {
-            id: 3,
-            Title: "Pulp Fiction",
-            ImagePath:
-                "https://m.media-amazon.com/images/I/5117V75KYYL._AC_.jpg",
-            Description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-            Director: "Quentin Tarantino",
-            Genre: "Action"
-        }
-    ]);
+    const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch('https://hidden-sea-19542.herokuapp.com/movies')
+            .then((response) => response.json())
+            .then((data) => {
+                const moviesFromApi = data.map((movie) => {
+                    return {
+                        id: movie._id,
+                        title: movie.Title,
+                        director: movie.Director,
+                        description: movie.Description,
+                        genre: movie.Genre
+                    };
+                });
+
+                setMovies(moviesFromApi);
+            });
+    }, []);
 
     if (selectedMovie) {
         return (
@@ -46,15 +36,17 @@ export const MainView = () => {
     } else {
         return (
             <div>
-                {movies.map((movie) => (
-                    <MovieCard
-                        key={movie.id}
-                        movie={movie}
-                        onMovieClick={(newSelectedMovie) => {
-                            setSelectedMovie(newSelectedMovie);
-                        }}
-                    />
-                ))}
+                {
+                    movies.map((movie) => (
+                        <MovieCard
+                            key={movie._id}
+                            movie={movie}
+                            onMovieClick={(newSelectedMovie) => {
+                                setSelectedMovie(newSelectedMovie);
+                            }}
+                        />
+                    ))
+                }
             </div>
         );
     }
