d841dd5177e848172b208c260e097b69a6165500
smeyer012 <115064549+smeyer012@users.noreply.github.com>
2023-02-26 03:40:42 -0600

    fetches API data from multiple endpoints for cross referencing

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,31 +3,79 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
+
     const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
 
-    useEffect(() => {
-        fetch('https://hidden-sea-19542.herokuapp.com/movies')
-            .then((response) => response.json())
-            .then((data) => {
-                const moviesFromApi = data.map((movie) => {
-                    return {
-                        id: movie._id,
-                        title: movie.Title,
-                        director: movie.Director,
-                        description: movie.Description,
-                        genre: movie.Genre
-                    };
-                });
+    const URLS = [
+        'https://hidden-sea-19542.herokuapp.com/movies',
+        'https://hidden-sea-19542.herokuapp.com/directors',
+        'https://hidden-sea-19542.herokuapp.com/genres',
+    ];
 
-                setMovies(moviesFromApi);
-            });
+    const datafromAPI = async () => {
+
+        const fetchedUrls = URLS.map(async (url) => {
+            const resp = await fetch(url);
+            return resp.json();
+        });
+
+        const [
+            movies,
+            directors,
+            genres,
+        ] = await Promise.all(fetchedUrls);
+
+        const moviesFromApi = movies.map((movie) => {
+
+            // const movieDirector = directors.find(({ _id }) => _id === movie.Director[0]) || {};
+            let movieDirector, directorList, addDirectors = "";
+            if (movie.Director.length == 1) {
+                directorList = directors.find(({ _id }) => _id === movie.Director[0]).Name;
+            }
+            else {
+                movie.Director.forEach((director) => {
+                    movieDirector = directors.find(({ _id }) => _id === director);
+                    addDirectors += movieDirector.Name + ', ';
+                });
+                directorList = addDirectors.replace(/, $/, '');
+            }
+
+            let movieGenre, genreList, addGenres = "";
+            if (movie.Genre.length == 1) {
+                genreList = genres.find(({ _id }) => _id === movie.Genre[0]).Name;
+            }
+            else {
+                movie.Genre.forEach((genre) => {
+                    movieGenre = genres.find(({ _id }) => _id === genre);
+                    addGenres += movieGenre.Name + ', ';
+                });
+                genreList = addGenres.replace(/, $/, '');
+            }
+
+            return {
+                id: movie._id,
+                title: movie.Title,
+                director: directorList,
+                description: movie.Description,
+                genre: genreList
+            };
+        });
+
+        setMovies(moviesFromApi);
+    }
+
+    useEffect(() => {
+        datafromAPI();
     }, []);
 
     if (selectedMovie) {
         return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+            <MovieView
+                movie={selectedMovie}
+                onBackClick={() => setSelectedMovie(null)}
+            />
         );
     }
 
