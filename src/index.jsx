74f472d869cb5b8ba508d9af841674a9cfaa9d8c
smeyer012 <115064549+smeyer012@users.noreply.github.com>
2023-03-03 17:38:06 -0600

    Adds bootstrap container around app

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
 import { createRoot } from 'react-dom/client';
+import Container from 'react-bootstrap/Container';
 import { MainView } from "./components/main-view/main-view";
 
-// Import statement to indicate that you need to bundle `./index.scss`
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
-// Main component (will eventually use all the others)
+// Main component 
 const MyFlixApplication = () => {
-    return <MainView />;
+    return (
+        <Container>
+            <MainView />
+        </Container>
+    );
 };
 
 // Finds the root of your app
