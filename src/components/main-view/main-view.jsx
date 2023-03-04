b53f00b4467c7eaa239fb8308488e5c35699280b
smeyer012 <115064549+smeyer012@users.noreply.github.com>
2023-03-03 20:17:37 -0600

    Customizes look of app

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Container from 'react-bootstrap/Container';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
@@ -79,52 +80,71 @@ export const MainView = () => {
     }, [token]);
 
     return (
-        <Row className="justify-content-md-center">
-            {!user ? (
-                <>
-                    <Col md={5}>
-                        <LoginView
-                            onLoggedIn={(user, token) => {
-                                setUser(user);
-                                setToken(token);
-                            }}
+        <>
+            <header className="py-3">
+                <Container>
+                    <Row>
+                        <Col md={8}>
+                            <div className="fs-2 mb-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-film" viewBox="0 0 16 16">
+                                    <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm4 0v6h8V1H4zm8 8H4v6h8V9zM1 1v2h2V1H1zm2 3H1v2h2V4zM1 7v2h2V7H1zm2 3H1v2h2v-2zm-2 3v2h2v-2H1zM15 1h-2v2h2V1zm-2 3v2h2V4h-2zm2 3h-2v2h2V7zm-2 3v2h2v-2h-2zm2 3h-2v2h2v-2z" />
+                                </svg>
+                                MyFlixApp
+                            </div>
+                        </Col>
+                        <Col md={4} className="d-flex align-items-start justify-content-end">
+                            {user &&
+                                <Button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</Button>
+                            }
+                        </Col>
+                    </Row>
+                </Container>
+            </header>
+            <Row className="justify-content-md-center">
+                {!user ? (
+                    <>
+                        <Col md={5} className="py-4">
+                            <LoginView
+                                onLoggedIn={(user, token) => {
+                                    setUser(user);
+                                    setToken(token);
+                                }}
+                            />
+                        </Col>
+                        <Col md={1}></Col>
+                        <Col md={5} className="py-4">
+                            <SignupView />
+                        </Col>
+                    </>
+                ) : selectedMovie ? (
+                    <Col md={6}>
+                        <MovieView
+                            movie={selectedMovie}
+                            onBackClick={() => setSelectedMovie(null)}
                         />
                     </Col>
-                    <Col md={1}></Col>
-                    <Col md={5}>
-                        <SignupView />
-                    </Col>
-                </>
-            ) : selectedMovie ? (
-                <Col md={6}>
-                    <MovieView
-                        movie={selectedMovie}
-                        onBackClick={() => setSelectedMovie(null)}
-                    />
-                </Col>
-            ) : movies.length === 0 ? (
-                <>
-                    <Button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</Button>
-                    <p>The list is empty!</p>
-                </>
-            ) : (
-                <>
-                    <Button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</Button>
-                    {
-                        movies.map((movie) => (
-                            <Col md={4} className="mb-4">
-                                <MovieCard
-                                    key={movie._id}
-                                    movie={movie}
-                                    onMovieClick={(newSelectedMovie) => {
-                                        setSelectedMovie(newSelectedMovie);
-                                    }}
-                                />
-                            </Col>
-                        ))
-                    }
-                </>
-            )}
-        </Row>
+                ) : movies.length === 0 ? (
+                    <>
+                        <p>The list is empty!</p>
+                    </>
+                ) : (
+                    <>
+                        {
+                            movies.map((movie) => (
+                                <Col md={4} className="mb-4">
+                                    <MovieCard
+                                        key={movie._id}
+                                        movie={movie}
+                                        onMovieClick={(newSelectedMovie) => {
+                                            setSelectedMovie(newSelectedMovie);
+                                        }}
+                                    />
+                                </Col>
+                            ))
+                        }
+                    </>
+                )}
+            </Row>
+        </>
     );
 };
